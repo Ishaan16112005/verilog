a3e30565749633154ed9ef82039f13b817d52e42
Ishaan16112005 <shettyishaan28@gmail.com>
2025-12-05 19:46:05 +0530

    Update SRAM.v

--- SRAM.v
+++ SRAM.v
@@ -1,41 +1,37 @@
- module sram #( 
-  parameter data_width = 8,
-  parameter mem_width = 1024,
-  parameter addr_width = 10
+module sram #(
+  parameter data_width = 4,
+  parameter mem_depth = 16,
+  parameter add_width = 4
 ) (
   input wire clk,
-  input wire rst_n,
-  input wire oe,
-  input wire we,
-  input wire cs,
+  input wire rst,
+  input wire chip_select,
+  input wire write_enable,
+  input wire read_enable,
   input wire [data_width-1:0] data_in,
-  input wire [addr_witdh-1: 0] addr,
-  output reg [data_width-1 : 0] data_out
+  input wire [add_width-1:0] addr_location,
+  output reg [data_width-1 :0 ] data_out
+
 );
-  reg [data_width -1 : 0] mem [0 : mem_width-1];
+  reg [data_width-1:0] mem [0: mem_depth-1];
   integer i;
-  always @(posedge clk or negedge rst_n)  begin
-    if (!rst_n) begin 
-      for (i = 0 ; i < mem_width ; i = i + 1) 
+  always @(posedge clk or negedge rst)  begin
+    if(!rst) begin
+      for(i = 0; i < mem_depth; i = i+1)
         mem[i] <= {data_width{1'b0}};
-      data_out <= {data_width{1'b0}};
 
-    
-    end else if (cs) begin 
-      if (we) begin 
-        mem[addr] <= data_in;
       end
-      else if (oe)  begin
-        data_out <= mem[addr];
-      end else  begin
-        data_out  <= {data_width{1'bz}};
-        
-      end 
-      // if the chip is  not selected
+      else if (chip_select) begin
+        if (write_enable) begin
+          mem[addr_location] <= data_in;
+        end
 
-    end else  begin
-      data_out <= {data_width{1'bz}};
+      else if (read_enable) begin
+        data_out <= mem[addr_location];
+      end
+    end
+    else begin
+      data_out <= {data_width{1'b0}};
     end
   end
-endmodule 
-    
+endmodule
