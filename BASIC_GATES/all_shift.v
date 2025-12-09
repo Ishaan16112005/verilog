45933471e126f1d937ee58933666361b66e9aec4
Ishaan16112005 <shettyshaan28@gmail.com>
2025-12-09 22:33:38 +0530

    BASIC GATES

---
+++ BASIC_GATES/all_shift.v
@@ -0,0 +1,19 @@
+module dynamic_shifters#(
+  parameter width = 8
+) (
+  input signed [width-1 : 0] a,
+  input [1:0] b,
+  output signed [width-1 : 0] r_shift,
+  output signed [width-1 : 0] l_shift 
+);
+  assign r_shift = a >>> b;
+  assign l_shift = a <<< b;
+endmodule
+
+// the above code is for arithmetic shift that preserves the sign
+
+
+ /*dynamic_shifters #(.width(8)) u_shift (
+ / ...........
+)*/
+// the lines 16, 17 and 18 are for the test bench part with the dynamic input
